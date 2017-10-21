aedb83436e3ec2ec1f9d15c9bf94ceb05849712a
HOYU <hoyu_z@163.com>
2017-10-21 17:46:55 +0800

    first

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using log4net;
+using log4net.Config;
+using System.IO;
+using System.Xml;
+using log4net.Repository;
+
+namespace CreateLog
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            ILoggerRepository repository = LogManager.CreateRepository("NETCoreRepository");
+            XmlConfigurator.Configure(repository, new FileInfo("log4net.config"));
+            ILog log = LogManager.GetLogger(repository.Name, "NETCorelog4net");
+
+            int i = 0;
+            while (i < 100000)
+            {
+                log.Info("GetJsonData(request/keyword)给定关键字不在字典中。");
+                i++;
+            }
+
+        }
+
+
+    }
+}
