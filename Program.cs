9ace9d11966f0f751071858bcf74ea04cf368d7c
HOYU <hoyu_z@163.com>
2017-10-23 17:43:46 +0800

    添加共享锁

--- Program.cs
+++ Program.cs
@@ -4,7 +4,6 @@ using log4net.Config;
 using System.IO;
 using System.Xml;
 using log4net.Repository;
-
 namespace CreateLog
 {
     public class Program
@@ -16,9 +15,10 @@ namespace CreateLog
             ILog log = LogManager.GetLogger(repository.Name, "NETCorelog4net");
 
             int i = 0;
-            while (i < 100000)
+            while (i<100)
             {
                 log.Info("GetJsonData(request/keyword)给定关键字不在字典中。");
+                log.Info("2017-10-11 00:42:45,874 [7] DEBUG MobileAdapterApi.Controllers.BaseController - Request fa116561-5df2-4ce8-9af0-a0937437fa1e {\"sender\":\"mp.test\",\"token\":\"\",\"timestamp\":\"20171011004342\",\"request\":{\"pagesize\":null,\"uvcode\":null,\"phone\":null,\"alias\":null,\"release2\":null,\"state\":null,\"areacode\":null,\"code\":null,\"level\":null,\"column7\":null,\"page\":null,\"address\":null,\"name\":null,\"release\":null,\"uvcid\":null,\"imgurl\":null}} hos");
                 i++;
             }
 
